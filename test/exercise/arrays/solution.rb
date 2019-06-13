9c1fc9b63f6fd92e078d57bd50c5a1faa6239c6e
Yakov <yakov.abdreimov@gmail.com>
2019-06-13 18:29:48 +0400

    arrays - binary search

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -6,7 +6,27 @@ module Exercise
       end
 
       def search(_array, _query)
-        _array.include?(_query) ? _array.index(_query) : -1
+        if _array.length == 0 || _query < _array[0] || _query > _array[_array.length - 1]
+          return -1
+        end
+        
+        binarySearch(_array, _query, 0, _array.length - 1)
+      end
+
+      def binarySearch(_array, _query, _left, _right)
+        mid = (_right + _left) / 2
+
+        if _query == _array[mid]
+          return mid
+        elsif (_right - _left) < 2
+          return -1
+        end
+
+        if _query > mid
+          binarySearch(_array, _query, mid + 1, _right)
+        else
+          binarySearch(_array, _query, _left, mid)
+        end
       end
     end
   end
