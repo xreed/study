49224646fe4e69b5e8d17740757803eeca8e472e
Yakov <yakov.abdreimov@gmail.com>
2019-06-13 20:01:34 +0400

    fp solution

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,11 +5,28 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(_array)
-        0
+        count = 0;
+
+        sum = _array.reduce 0 do |sum, film|
+          if film["rating_kinopoisk"].to_s.empty? || film["rating_kinopoisk"] == '0' ||
+            film["country"].to_s.empty? || film["country"].to_s.count(',') < 1
+            sum
+          else
+            count += 1
+            sum + film["rating_kinopoisk"].to_f
+          end
+        end
+        sum / count
       end
 
       def chars_count(_films, _threshold)
-        0
+        _films.reduce 0 do |sum, film|
+          if film["rating_kinopoisk"].to_s.empty? || film["rating_kinopoisk"].to_f < _threshold
+            sum
+          else
+            sum + film["name"].count('и')
+          end
+        end
       end
     end
   end
